1c7fe403c5bbc7409762a881d98ab36b5a4145e9
teheidoma <novakovskii2002@gmail.com>
2019-10-16 20:00:04 +0300

    init

---
+++ src/main/kotlin/com/teheidoma/nicknamecolor/NicknamecolorApplication.kt
@@ -0,0 +1,11 @@
+package com.teheidoma.nicknamecolor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NicknamecolorApplication
+
+fun main(args: Array<String>) {
+    runApplication<NicknamecolorApplication>(*args)
+}
---
+++ src/test/kotlin/com/teheidoma/nicknamecolor/NicknamecolorApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.teheidoma.nicknamecolor
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class NicknamecolorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
